c5a9289e2bb85aea38720e7b6368abb4c4648ef2
juanxbini <juanxbini@gmail.com>
2025-03-05 09:48:39 -0300

    Configurara Redux en main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
 )
